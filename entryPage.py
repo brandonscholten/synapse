d8867413299587cd7fa4a4669de585c7c27c670b
aishaahammed <143667436+aishaahammed@users.noreply.github.com>
2024-03-10 07:59:00 -0400

    background, got rid of subprocess
    background, got rid of subprocess

--- entryPage.py
+++ entryPage.py
@@ -1,6 +1,18 @@
 import streamlit as st
 from streamlit_extras.switch_page_button import switch_page
 
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] {{
+background-image: url("https://i.pinimg.com/1200x/47/26/f1/4726f134466769d03b957290290c101f.jpg");
+background-size: cover;
+}}
+
+
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 css ='''
 <style>
     [data-testid="stSidebar"] {
